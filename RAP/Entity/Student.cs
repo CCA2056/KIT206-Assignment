342f0e11bc667f29ede6399906f67a6ad3e5dc3d
David Yang <2501850253@qq.com>
2021-05-12 12:13:28 +0800

    add student entity class

---
+++ RAP/Entity/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RAP
+{
+    class Student : Researcher
+    {
+        public string Degree { get; set; }
+    }
+}
