83a22ed78c6ecdde5c5a6e341b8cbb381fa6e9df
CCA2056 <83960122+CCA2056@users.noreply.github.com>
2021-05-14 14:53:57 +0800

    test

---
+++ Entity/Staff.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RAP
+{
+    class Staff : Researcher
+    {
+        public int PublicationCount3yr()
+        {
+            return Publication.Count(p => p.Year > DateTime.Today.Year - 3);
+        }
+
+        public float ThreeYearAverage()
+        {
+            return (PublicationCount3yr()) * 3;
+        }
+
+        public float Performance()
+        {
+            return 0; //PublicationCount3yr() ;
+        }
+    }
+}
