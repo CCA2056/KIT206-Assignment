b362c5781e54454beb44409528e57075595faeeb
Eric0420 <84004896+Eric0420@users.noreply.github.com>
2021-05-25 09:42:27 +0800

    upload

---
+++ Entity/Staff.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RAP
+{
+    public class Staff : Researcher
+    {
+        public List<Student> Supervision { get; set; }
+
+        public string Performance { get; set; }
+        public int PublicationCount
+        {
+            get
+            {
+                return Publication.Count;
+            }
+            
+        }
+
+        public int Count3year
+        {
+            get
+            {
+                return Publication.Count(p => p.Year > DateTime.Today.Year - 3);
+            }
+        }
+
+        public float ThreeYearAverage
+        {
+            get
+            {
+                return Count3year/3;
+            }
+            
+        }
+    }
+}
